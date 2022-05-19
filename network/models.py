33852db3d8c0b8b4895c6a7224a8aecf4f25b6a5
kftlfd <kftlfd@ex.ua>
2022-05-19 19:18:40 +0300

    add basic models

--- network/models.py
+++ network/models.py
@@ -1,6 +1,26 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils import timezone
 
 
 class User(AbstractUser):
     pass
+
+
+class Post(models.Model):
+    user = models.ForeignKey('User', on_delete=models.CASCADE, related_name='user_posts')
+    content = models.TextField()
+    time = models.DateTimeField(auto_now_add=timezone.now())
+
+
+class Reaction(models.Model):
+    user = models.ForeignKey('User', on_delete=models.CASCADE, related_name='user_reactions')
+    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='post_reactions')
+    type = models.IntegerField()
+
+
+class Comment(models.Model):
+    user = models.ForeignKey('User', on_delete=models.CASCADE, related_name='user_comments')
+    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='post_comments')
+    content = models.TextField()
+    time = models.DateTimeField(auto_now_add=timezone.now())
