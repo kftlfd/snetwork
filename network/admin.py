c801f408ce8dca343cb52ede39a848ce01ff0fad
kftlfd <kftlfd@ex.ua>
2022-05-20 12:49:04 +0300

    admin control of models

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import *
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "username")
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "time")
+
+class ReactionAdmin(admin.ModelAdmin):
+    list_display = ("id", "post", "user", "type")
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("id", "post", "user", "time")
 
 # Register your models here.
+admin.site.register(User, UserAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Reaction, ReactionAdmin)
+admin.site.register(Comment, CommentAdmin)
